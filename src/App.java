7a68897f2e6d86c46c7e6836e1a3ea85fe445211
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-09-21 22:39:36 -0300

    :sparkles: added study about declarations names and the importance

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
 import package1.Dog;
 
+import java.util.List;
+
+import package1.Cell;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        Dog cachorro = new Dog();
-        cachorro.bark();
+
     }
 } //jar war e ear
---
+++ src/package1/Cell.java
@@ -0,0 +1,46 @@
+package package1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cell {
+
+    public boolean isFlagged = false;
+    public int value;
+
+    public Cell(boolean isFlagged, int value) {
+        this.isFlagged = isFlagged;
+        this.value = value;
+    }
+
+    public void verifyFlagged(boolean isFlagged) {
+        if (value == 4) {
+            isFlagged = true;
+        }
+    }
+
+    /*public List<int[]> getThem() {
+        List<int[]> list1 = new ArrayList<>();
+        for (int[] x : theList) {
+            if (x[0] == 4) {
+                list1.add(x);
+            }
+        }
+
+        return list1;
+    }*/
+
+    //consegues perceber a diferença deste codigo acima para o codigo abaixo?
+    //este é o poder de dar significado aos nomes, tanto de varives, como de classes e etc
+    //por isso, quando for declarar algo, pense bem antes de fazelo
+
+    public List getFllagedCell() {
+        List<Cell> flaggedCells = new ArrayList<Cell>();
+        for (Cell cell : gameBoard) {
+            if (cell.isFlagged()) {
+                flaggedCells.add(cell);
+            }
+        }
+        return flaggedCells;
+    }
+}
